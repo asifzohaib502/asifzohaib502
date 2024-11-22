65ae64b017adbc47577cded80105deaff4c693d4
asifzohaib502 <asifzohaib502502@gmail.com>
2024-11-22 13:47:28 +0000

    Create grades.py

---
+++ grades.py
@@ -0,0 +1,14 @@
+if mark >= 70:
+    print("Achieved Grade: A")
+elif mark >= 60:
+    print("Achieved Grade: B")
+elif mark >= 50:
+    print("Achieved Grade: C")
+elif mark >= 40:
+    print("Achieved Grade: D")
+elif mark >= 30:
+    print("Achieved Grade: E")
+elif mark >= 20:
+    print("Achieved Grade: F")
+else:
+    print("Grade not valid")
